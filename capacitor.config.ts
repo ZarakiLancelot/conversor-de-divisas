2fff71e27c6bc56fc22334eaf0e82442e9ad261f
ZarakiLancelot <kenpachi2783@gmail.com>
2023-11-15 19:09:19 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Conversor de Divisas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
